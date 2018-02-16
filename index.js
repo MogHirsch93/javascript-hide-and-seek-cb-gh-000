ff87c2546eb4c36bfc74d4f736264cc3d93b1463
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 14:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function getFirstSelector(selector){
   return document.querySelector(selector);
 }
+
 function nestedTarget(selector){
   return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let lis = document.getElementBy('app').querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < lis.length; i++){
+    return lis[i] + parseInt(n);
+  }
 }
