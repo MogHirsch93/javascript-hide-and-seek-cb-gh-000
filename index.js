c5925ea6666b4970c787cc9939709ef7b635fb7c
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 14:46:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(selector){
-  return document.getElementById('grand-node').querySelector('div div div div')
+  return document.getElementById('grand-node').querySelector('div div div div div')
 }
