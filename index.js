e77aa9168b4404687d8b79d80067f78b00a4ff00
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 14:46:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(selector){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+  return lis;
+}
+
+function deepestChild(selector){
+  return document.getElementById('grand-node').querySelector('div div div div div')
+}
